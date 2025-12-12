71e13b2d6974c279d42d15e65eee1ca90fd34451
JefkeB <37012100+JefkeB@users.noreply.github.com>
2025-12-12 08:14:11 +0100

    Add source code

---
+++ source/UsbSerialPortPluginMonitor/UsbSerialPortPluginMonitor/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace UsbSerialPortPluginMonitor
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.log_listBox = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(104, 54);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(100, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeButton.Location = new System.Drawing.Point(391, 221);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // log_listBox
+            // 
+            this.log_listBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.log_listBox.FormattingEnabled = true;
+            this.log_listBox.Location = new System.Drawing.Point(12, 12);
+            this.log_listBox.Name = "log_listBox";
+            this.log_listBox.Size = new System.Drawing.Size(454, 199);
+            this.log_listBox.TabIndex = 1;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 256);
+            this.Controls.Add(this.log_listBox);
+            this.Controls.Add(this.closeButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Usb SerialPort PluginMonitor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.mainForm_Shown);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.ListBox log_listBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ source/UsbSerialPortPluginMonitor/UsbSerialPortPluginMonitor/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+
+namespace UsbSerialPortPluginMonitor
+{
+    public partial class mainForm : Form
+    {
+        private bool mustClose = false;
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            //
+            notifyIcon.Visible = true;
+            notifyIcon.BalloonTipText = " ";
+            notifyIcon.Icon = new Icon(this.Icon, 40, 40);
+            notifyIcon.BalloonTipIcon = ToolTipIcon.Info;
+
+            //
+            SerialPortService.PortsChanged += (sender1, changedArgs) => { HandlePortChanges(changedArgs); };
+        }
+
+
+        // Changes in the serial ports on the pc
+        //
+        //
+        void HandlePortChanges(PortsChangedArgs args)
+        {
+            string Text = "";
+            string description = "";
+            var Ports = COMPortInfo.Info.Get();
+
+            // concat multiple changes
+            foreach (string name in args.SerialPorts)
+            {
+                Console.WriteLine(args.EventType + " " + name);
+                if (Text != "")
+                {
+                    Text += " \r\n ";
+                }
+                
+                description = "";
+
+                if (args.EventType == EventType.Insertion)
+                {
+                    foreach (COMPortInfo.Info comPort in Ports)
+                    {
+                        if (comPort.Name == name)
+                        {
+                            description = comPort.Description;
+                        }
+                    }
+                }
+
+                Text = args.EventType + " " + name + " " + description;
+            }
+
+            if (Text != "")
+            {
+                notifyIcon.BalloonTipTitle = Text;
+                notifyIcon.ShowBalloonTip(1500);
+            }
+
+            if (Visible)
+            {
+                // update Ports_comboBox list
+                log_listBox.Invoke((MethodInvoker)(() =>
+                {
+                    updateComportList();
+                }
+                ));
+            }
+        }
+
+
+        // 
+        //
+        //
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("notifyIcon_MouseDoubleClick");
+            ShowWindow();
+        }
+
+
+        //
+        //
+        //
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+
+        //
+        //
+        //
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mustClose = true;
+            Close();
+        }
+
+
+        //
+        //
+        //
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowWindow();
+        }
+
+
+        //
+        //
+        //
+        private void ShowWindow()
+        {
+            if (Visible == false)
+            {
+                WindowState = FormWindowState.Normal;
+                updateComportList();
+                Show();
+            }
+        }
+
+
+        // update list with friendly names of comports
+        //
+        //
+        private void updateComportList()
+        {
+            lock (log_listBox)
+            {                
+                var sortedPorts = COMPortInfo.Info.Get().OrderBy(Name => Convert.ToUInt32(Name.Name.Replace("COM", string.Empty)));
+                string Text;
+                log_listBox.Items.Clear();
+                foreach (COMPortInfo.Info comPort in sortedPorts)
+                {
+                    Text = string.Format("{0} – {1}", comPort.Name, comPort.Description);
+                    Console.WriteLine(Text);
+                    log_listBox.Items.Add(Text);
+                }
+            }
+        }
+
+
+        //
+        //
+        //
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // closed -> not by user clicking close button [x] form
+            if ((e.CloseReason == CloseReason.UserClosing) && (mustClose == true))
+            {
+                return;
+            }
+
+            // just hide the form
+            e.Cancel = true;
+            Hide();
+        }
+
+
+        //
+        //
+        //
+        private void mainForm_Shown(object sender, EventArgs e)
+        {
+            // hide form at start of program
+            Hide();
+        }
+    }
+}
